b5797b94cf74c5718583e89178bba76cc623b7bd
Marc Cataford <mcat@riseup.net>
2021-10-20 21:24:02 -0400

    feat: selection cli, fetch assigned items

---
+++ src/types.ts
@@ -0,0 +1,34 @@
+export interface AssignedIssueNode {
+    number: number
+    title: string
+    author: {
+        login: string
+    }
+    publishedAt: string
+    reviewDecision: string
+    updatedAt: string
+    createdAt: string
+    url: string
+    repository: {
+        name: string
+    }
+}
+
+export interface AssignedIssue {
+    number: number
+    title: string
+    author: string
+    publishedAt: Date
+    updatedAt: Date
+    createdAt: Date
+    reviewDecision: string
+    repository: string
+    url: string
+}
+export interface State {
+    selected: number
+    lookup: Map<number, AssignedIssue>
+    visible: number[]
+    query: string
+}
+export type AssignedIssueByRepoMap = Map<string, Array<AssignedIssue>>
